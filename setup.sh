f5fed3ed2cd2ecea85011672906cfd09b471c6ac
kny-i <kny_i@keio.jp>
2022-11-05 16:49:16 +0900

    edit vimrcr

--- setup.sh
+++ setup.sh
@@ -4,4 +4,12 @@
 echo ---begin----
 ln -sf ~/settings/.vimrc ~/.vimrc
 ln -sf ~/settings/.zshrc ~/.zshrc
+
+mkdir -p ~/.vim/pack/themes/start
+
+cd ~/.vim/pack/themes/start
+git clone https://github.com/dracula/vim.git dracula
+
+
 source ~/.zshrv
+
